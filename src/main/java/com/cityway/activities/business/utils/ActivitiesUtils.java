aaa069f40227a0cc209f6e0cd053c11ea99fa3f1
Alba Batista <devalbabatista@gmail.com>
2023-12-29 13:48:17 +0100

    modify capitalize method + create ActivitiesUtilsTest

--- src/main/java/com/cityway/activities/business/utils/ActivitiesUtils.java
+++ src/main/java/com/cityway/activities/business/utils/ActivitiesUtils.java
@@ -2,7 +2,6 @@ package com.cityway.activities.business.utils;
 
 import java.util.function.UnaryOperator;
 
-
 /**
  * The Class ActivitiesUtils.
  */
@@ -10,10 +9,27 @@ public class ActivitiesUtils {
 
 	public static String EMPTY = "";
 
-	public static String capitalize(String word) {
-		UnaryOperator<String> capitalizeWord = t -> t.substring(0, 1).toUpperCase() + t.substring(1);
-		return (word.isBlank())? EMPTY : capitalizeWord.apply(word).trim();
- 	
+	public static String SPACE = " ";
+
+	public static String capitalize(String phrase) {
+
+		StringBuilder result = new StringBuilder();
+
+		if (phrase !=null && !phrase.isBlank()) {
+			
+			String[] words = phrase.trim().split(" ");
+
+			UnaryOperator<String> capitalizeWord = t -> t.substring(0, 1).toUpperCase() + t.substring(1);
+
+			for (int i = 0; i < words.length; i++) {
+				result.append(capitalizeWord.apply(words[i]));
+				result.append(SPACE);
+			}
+
+		}
+
+		return result.toString().trim();
+
 	}
 
 }
