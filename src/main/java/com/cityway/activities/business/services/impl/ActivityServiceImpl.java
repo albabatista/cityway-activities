c9fb0f52bdb60b347335ee6bdd587a3f034243b6
Alba Batista <devalbabatista@gmail.com>
2023-12-30 17:59:40 +0100

    remove space

--- src/main/java/com/cityway/activities/business/services/impl/ActivityServiceImpl.java
+++ src/main/java/com/cityway/activities/business/services/impl/ActivityServiceImpl.java
@@ -124,8 +124,6 @@ public class ActivityServiceImpl implements ActivityService {
 		return convertIntegrationToBusinessList(activitiesList);
 	}
 
-	
-
 
 	// ***************************************************************
 	//
