ddebfa2fd7ad73ebd277111a1927e567f8a7202f
Alba Batista <devalbabatista@gmail.com>
2023-12-30 18:17:11 +0100

    add update test

--- src/test/java/com/cityway/activities/business/services/impl/ActivityServiceImplTest.java
+++ src/test/java/com/cityway/activities/business/services/impl/ActivityServiceImplTest.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -23,6 +24,7 @@ import com.cityway.activities.business.models.Activity;
 import com.cityway.activities.integration.mappers.ActivityMapper;
 import com.cityway.activities.integration.models.ActivityDto;
 import com.cityway.activities.integration.repositories.ActivityRepository;
+import com.cityway.activities.presentation.exceptions.ActivityNotFoundException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @ExtendWith(MockitoExtension.class)
@@ -70,7 +72,7 @@ class ActivityServiceImplTest {
 	@ParameterizedTest
 	@EmptySource
 	@ValueSource(strings = "658da42b7e5c3c47845cbfe8")
-	public void readTest(String id) {
+	void readTest(String id) {
 		when(activityMapper.activityToDto(activity)).thenReturn(activityDto);
 		activityServiceImpl.create(activity);
 		
@@ -82,6 +84,28 @@ class ActivityServiceImplTest {
 			when(activityRepository.findById(id)).thenReturn(Optional.empty());
 			assertNull(activityServiceImpl.read(id));
 		}
+
+	}
+	
+	@ParameterizedTest
+	@NullSource
+	@EmptySource
+	@ValueSource(strings = "658da42b7e5c3c47845cbfe8")
+	void updateTest(String id) {
+		if (id == null) {
+			assertThrows(IllegalArgumentException.class, () -> activityServiceImpl.update(null));
+			
+		}else if (id.isEmpty()) {
+			activity = new Activity();
+			activity.setId("0");
+			assertThrows(ActivityNotFoundException.class, () -> activityServiceImpl.update(activity));
+		}else {
+			when(activityRepository.existsById(id)).thenReturn(true);
+			when(activityMapper.activityToDto(activity)).thenReturn(activityDto);
+			activityServiceImpl.update(activity);
+			verify(activityRepository, times(1)).save(activityDto);
+		}
+		
 
 	}
 
